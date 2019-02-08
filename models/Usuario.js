56a504df4068ed19a52073f9130e408fff17b727
Marco Monter <monter.marco@gmail.com>
2019-02-08 14:57:04 -0600

    Signup

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,14 +1,16 @@
+// models/user.js
+
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
   email: String,
-  usuario: String,
+  username: String,
   password: String,
   tiendas: [ {type: String} ],
   categorias: [{type: String}],
-  compras: [{type: String}]  
+  compras: [{type: String}]
 });
 
-const Usuario = mongoose.model('Usuario', userSchema);
-module.exports = Usuario;
+const User = mongoose.model('User', userSchema);
+module.exports = User;
